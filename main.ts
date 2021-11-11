aa588bc3ebdb4acc8bc5650ce083ecd46e2ca9cc
jafadgex <92938346+jafadgex@users.noreply.github.com>
2021-11-11 10:19:11 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    Counter += 1
+    basic.showNumber(Counter)
+})
+input.onButtonPressed(Button.AB, function () {
+    Counter = 0
+    basic.showNumber(Counter)
+})
+input.onButtonPressed(Button.B, function () {
+    Counter += -1
+    basic.showNumber(Counter)
+})
+let Counter = 0
+Counter = 0
+basic.showNumber(Counter)
